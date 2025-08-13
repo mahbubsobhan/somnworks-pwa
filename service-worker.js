b856bef060edbde7001a45e98c1071b3c23bcff9
Maruf <mahbubsobhan3636@gmail.com>
2025-08-13 23:09:26 +0600

    Update PWA - keep installed display before redirect

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,21 @@
 self.addEventListener('install', event => {
-  event.waitUntil(self.skipWaiting());
+    event.waitUntil(
+        caches.open('somnworks-cache').then(cache => {
+            return cache.addAll([
+                './',
+                './index.html',
+                './manifest.json',
+                './icons/icon-192.png',
+                './icons/icon-512.png'
+            ]);
+        })
+    );
 });
 
-self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
 });
